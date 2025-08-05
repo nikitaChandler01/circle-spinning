cb52e9da1e883e7c40851ab94a0e6f64ab178c1d
nm.berdnik <nm.berdnik@tensor.ru>
2025-08-05 14:43:53 +0700

    feat: highlight title done

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './MainPage.scss';
 import { EventsWidget } from '@widgets/EventsWidget';
+import PageGrid from './PageGrid';
+import { HighlightTitle } from '@shared/ui/Typography/HighlightTitle';
 
 const MainPage = () => {
+  const pageTitle = 'Исторические \n даты';
+
   return (
     <div className="main-page">
+      <PageGrid />
       <div className="main-page__widget-wrapper">
+        <HighlightTitle text={pageTitle} />
         <EventsWidget />
       </div>
     </div>
