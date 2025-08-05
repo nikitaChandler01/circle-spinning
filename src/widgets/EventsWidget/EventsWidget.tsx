c4bb216bdd05c90bd481d5114dc41ea4715e9065
nikitaBerdnik <berdnik.nikita01@gmail.com>
2025-08-05 20:07:06 +0700

    refactor: logic decomposed

--- src/widgets/EventsWidget/EventsWidget.tsx
+++ src/widgets/EventsWidget/EventsWidget.tsx
@@ -4,39 +4,14 @@ import Carousel from '@shared/ui/Carousel/Carousel';
 import gsap from 'gsap';
 import React, { useEffect, useRef, useState } from 'react';
 import './EventsWidget.scss';
+import { useAnimatedEventsWidget } from './UseAnimatedEventsWidget';
 
 interface IEventsWidget {
   ageId: number;
 }
 
 const EventsWidget = ({ ageId }: IEventsWidget) => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const fadeTween = useRef<gsap.core.Tween | null>(null);
-  const timeOfAnimation = 0.3;
-  const [currentAgeId, setCurrentAgeId] = useState(ageId);
-  const [events, setEvents] = useState(EVENTS_MOCKS[ageId]);
-
-  useEffect(() => {
-    if (ageId === currentAgeId) return;
-    fadeTween.current?.kill();
-    fadeTween.current = gsap.to(containerRef.current, {
-      opacity: 0,
-      duration: timeOfAnimation,
-      onComplete: () => {
-        setCurrentAgeId(ageId);
-        setEvents(EVENTS_MOCKS[ageId]);
-        fadeTween.current = gsap.fromTo(
-          containerRef.current,
-          { opacity: 0 },
-          { opacity: 1, duration: timeOfAnimation },
-        );
-      },
-    });
-  }, [ageId]);
-
-  useEffect(() => {
-    gsap.set(containerRef.current, { opacity: 1 });
-  }, []);
+  const { containerRef, events } = useAnimatedEventsWidget({ ageId });
 
   return (
     <div ref={containerRef}>
---
+++ src/widgets/EventsWidget/UseAnimatedEventsWidget.ts
@@ -0,0 +1,42 @@
+import { EVENTS_MOCKS } from '@shared/mocks/EventsMocks';
+import gsap from 'gsap';
+import { useEffect, useRef, useState } from 'react';
+
+interface IUseAnimatedEventsWidget {
+  ageId: number;
+}
+
+export const useAnimatedEventsWidget = ({ ageId }: IUseAnimatedEventsWidget) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const fadeTween = useRef<gsap.core.Tween | null>(null);
+  const timeOfAnimation = 0.3;
+  const [currentAgeId, setCurrentAgeId] = useState(ageId);
+  const [events, setEvents] = useState(EVENTS_MOCKS[ageId]);
+
+  useEffect(() => {
+    if (ageId === currentAgeId) return;
+    fadeTween.current?.kill();
+    fadeTween.current = gsap.to(containerRef.current, {
+      opacity: 0,
+      duration: timeOfAnimation,
+      onComplete: () => {
+        setCurrentAgeId(ageId);
+        setEvents(EVENTS_MOCKS[ageId]);
+        fadeTween.current = gsap.fromTo(
+          containerRef.current,
+          { opacity: 0 },
+          { opacity: 1, duration: timeOfAnimation },
+        );
+      },
+    });
+  }, [ageId]);
+
+  useEffect(() => {
+    gsap.set(containerRef.current, { opacity: 1 });
+  }, []);
+
+  return {
+    containerRef,
+    events,
+  };
+};
