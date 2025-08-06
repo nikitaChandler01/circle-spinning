b536a58baf4cb68bb721af1f299c8658b813e6dd
nikitaBerdnik <berdnik.nikita01@gmail.com>
2025-08-06 21:30:36 +0700

    feat: вернул старую логику анимации вращения

--- src/widgets/TimelineSelect/service/animation.service.ts
+++ src/widgets/TimelineSelect/service/animation.service.ts
@@ -23,33 +23,48 @@ export const increaseTarget = (increasingElem: HTMLDivElement | null) =>
 export const decreaseTarget = (increasingElem: HTMLDivElement | null) =>
   gsap.to(increasingElem, decreasedSelectedItemStyles);
 
-export const moveForPath = ({ path, elements, startPoint, endPoint, duration = 0, ease }: IMoveForPath) => {
-  const totalPoints = elements.length;
-  const phaseShift = 1 / totalPoints; // это фазовый сдвиг на 60 градусов 1/6 окружности
-
-  return gsap.to(elements, {
+export const moveForPath = ({ path, elements, startPoint, endPoint, duration = 0, ease }: IMoveForPath) =>
+  gsap.to(elements, {
     motionPath: {
       path: path,
       align: path,
       autoRotate: false,
       alignOrigin: [0.5, 0.5],
-      start: (i) => {
-        const index = i + 0.5;
-        return (((index - startPoint + totalPoints) % totalPoints) / totalPoints + phaseShift) % 1;
-      },
-      end: (i) => {
-        const index = i + 0.5;
-        const startPos = (((index - startPoint + totalPoints) % totalPoints) / totalPoints + phaseShift) % 1;
-        const endPos = (((index - endPoint + totalPoints) % totalPoints) / totalPoints + phaseShift) % 1;
-
-        let delta = endPos - startPos;
-
-        if (delta > 0.5) delta -= 1;
-        if (delta < -0.5) delta += 1;
-        return startPos + delta;
-      },
+      start: (i) => (i + 0.5 - startPoint + 1) / 6,
+      end: (i) => (i + 0.5 - endPoint + 1) / 6,
     },
     duration,
     ease,
   });
-};
+
+// export const moveForPath = ({ path, elements, startPoint, endPoint, duration = 0, ease }: IMoveForPath) => {
+//   const totalPoints = elements.length;
+//   const phaseShift = 1 / totalPoints; // это фазовый сдвиг на 60 градусов 1/6 окружности
+//   const isEdgeToEdge = Math.abs(startPoint - endPoint) === totalPoints - 1;
+
+//   return gsap.to(elements, {
+//     motionPath: {
+//       path: path,
+//       align: path,
+//       autoRotate: false,
+//       alignOrigin: [0.5, 0.5],
+//       start: (i) => {
+//         const index = i + 0.5;
+//         return (((index - startPoint + totalPoints) % totalPoints) / totalPoints + phaseShift) % 1;
+//       },
+//       end: (i) => {
+//         const index = i + 0.5;
+//         const startPos = (((index - startPoint + totalPoints) % totalPoints) / totalPoints + phaseShift) % 1;
+//         const endPos = (((index - endPoint + totalPoints) % totalPoints) / totalPoints + phaseShift) % 1;
+
+//         let delta = endPos - startPos;
+//         if (!isEdgeToEdge) return startPos + delta;
+//         if (delta > 0.5) delta -= 1;
+//         if (delta < -0.5) delta += 1;
+//         return startPos + delta;
+//       },
+//     },
+//     duration,
+//     ease,
+//   });
+// };
